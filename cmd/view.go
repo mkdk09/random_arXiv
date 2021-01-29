6d0ae34f18101a448335f788cbcb3ea41efb6c59
mkdk09 <mkdk099@gmail.com>
2021-01-29 17:27:57 +0900

    XMLを解析した結果を表示

--- cmd/view.go
+++ cmd/view.go
@@ -16,12 +16,81 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+type Feed struct {
+	XMLName xml.Name `xml:"feed"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Link    struct {
+		Text string `xml:",chardata"`
+		Href string `xml:"href,attr"`
+		Rel  string `xml:"rel,attr"`
+		Type string `xml:"type,attr"`
+	} `xml:"link"`
+	Title struct {
+		Text string `xml:",chardata"`
+		Type string `xml:"type,attr"`
+	} `xml:"title"`
+	ID           string `xml:"id"`
+	Updated      string `xml:"updated"`
+	TotalResults struct {
+		Text       string `xml:",chardata"`
+		Opensearch string `xml:"opensearch,attr"`
+	} `xml:"totalResults"`
+	StartIndex struct {
+		Text       string `xml:",chardata"`
+		Opensearch string `xml:"opensearch,attr"`
+	} `xml:"startIndex"`
+	ItemsPerPage struct {
+		Text       string `xml:",chardata"`
+		Opensearch string `xml:"opensearch,attr"`
+	} `xml:"itemsPerPage"`
+	Entry struct {
+		Text      string `xml:",chardata"`
+		ID        string `xml:"id"`
+		Updated   string `xml:"updated"`
+		Published string `xml:"published"`
+		Title     string `xml:"title"`
+		Summary   string `xml:"summary"`
+		Author    struct {
+			Text string `xml:",chardata"`
+			Name string `xml:"name"`
+		} `xml:"author"`
+		Comment struct {
+			Text  string `xml:",chardata"`
+			Arxiv string `xml:"arxiv,attr"`
+		} `xml:"comment"`
+		JournalRef struct {
+			Text  string `xml:",chardata"`
+			Arxiv string `xml:"arxiv,attr"`
+		} `xml:"journal_ref"`
+		Link []struct {
+			Text  string `xml:",chardata"`
+			Href  string `xml:"href,attr"`
+			Rel   string `xml:"rel,attr"`
+			Type  string `xml:"type,attr"`
+			Title string `xml:"title,attr"`
+		} `xml:"link"`
+		PrimaryCategory struct {
+			Text   string `xml:",chardata"`
+			Arxiv  string `xml:"arxiv,attr"`
+			Term   string `xml:"term,attr"`
+			Scheme string `xml:"scheme,attr"`
+		} `xml:"primary_category"`
+		Category []struct {
+			Text   string `xml:",chardata"`
+			Term   string `xml:"term,attr"`
+			Scheme string `xml:"scheme,attr"`
+		} `xml:"category"`
+	} `xml:"entry"`
+}
 
 // viewCmd represents the view command
 var viewCmd = &cobra.Command{
@@ -40,6 +109,36 @@ func main(cmd *cobra.Command, args []string) {
 	baseUrl := "http://export.arxiv.org/api/query?search_query=cat:cs.AI&sortBy=lastUpdatedDate&sortOrder=descending&max_results=1&start=29500"
 	data := httpGet(baseUrl)
 	fmt.Println(data)
+	result := Feed{}
+	err := xml.Unmarshal([]byte(data), &result)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	// タイトル
+	fmt.Println(result.Entry.Title)
+	// 発行日
+	fmt.Println(result.Entry.Published)
+	// 更新日
+	fmt.Println(result.Entry.Updated)
+	// サマリー
+	fmt.Println(result.Entry.Summary)
+	// 著者
+	fmt.Println(result.Entry.Author.Name)
+	// リンク
+	// fmt.Println(result.Entry.Link[0].Href)
+	// pdfリンク
+	// fmt.Println(result.Entry.Link[1].Href)
+	for _, link := range result.Entry.Link {
+		fmt.Println(link.Href)
+	}
+	// 主要カテゴリ
+	fmt.Println(result.Entry.PrimaryCategory.Term)
+	// サブカテゴリー
+	for _, category := range result.Entry.Category {
+		fmt.Println(category.Term)
+	}
 }
 
 func httpGet(url string) string {
