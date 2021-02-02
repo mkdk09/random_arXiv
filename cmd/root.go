c921fb7ba7909100db6e843e8b8e24b0ed75bf60
mkdk09 <mkdk099@gmail.com>
2021-02-02 23:17:39 +0900

    ヘルプコマンドを実行した時の説明を追加

--- cmd/root.go
+++ cmd/root.go
@@ -34,12 +34,10 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "random_arXiv",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `
+random_arXivはarXivの機械学習関連の論文をランダムで表示するCLIです．
+6つの種類のカテゴリを選択でき，
+タイトル，サマリー，ページリンクなどを表示します．`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -68,7 +66,6 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	// サブコマンドでも共通で利用できるグローバルな引数(category)の設定
-	// 現在デフォルトはcs.AIだが，ランダムにするかも
 	rootCmd.PersistentFlags().StringVarP(&globalFlags.category, "cat", "c", "random", "Category : cs.AI, cs.LG, cs.CL, cs.CV, cs.NE, stat.ML")
 }
 
--- cmd/view.go
+++ cmd/view.go
@@ -99,12 +99,10 @@ type Feed struct {
 var viewCmd = &cobra.Command{
 	Use:   "view",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `
+random_arXivはarXivの機械学習関連の論文をランダムで表示するCLIです．
+6つの種類のカテゴリを選択でき，
+タイトル，サマリー，ページリンクなどを表示します．`,
 	Run: main,
 }
 
